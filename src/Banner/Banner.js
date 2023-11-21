5919dc897fb00f0a598dbbe5fd9f485ac599088b
9072548893 <137638716+9072548893@users.noreply.github.com>
2023-11-21 17:03:51 +0530

    banner page description added

---
+++ src/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='banner'>
+     <div className='content'>
+        <h1 className='title'>movie name</h1>
+        <div className='banner-buttons'>
+            <button className='buttonn'>play</button>
+            <button className='buttonn'>mylist</button>
+        </div>
+        <h1 className='description'>In a small coastal town nestled between rolling hills and the vast expanse of the ocean, life unfolded at its own leisurely pace. The cobblestone streets meandered through the heart of the community, lined with charming cottages adorned with colorful flower gardens. Each morning, the aroma of freshly brewed coffee wafted through the air from the local café, inviting residents to start their day with a warm cup and friendly conversation. As the sun dipped below the horizon, the sky transformed into a canvas of vibrant hues, casting a soft glow over the tranquil waters. This town, with its close-knit community and picturesque surroundings, was a haven where time seemed to slow down, allowing its residents to savor the simple joys of life</h1>
+
+     </div>
+
+      
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <img className='logo' src="https://img.icons8.com/?size=48&id=20519&format=png" alt="netflix logo" />
+        <img className='avatar' src="https://img.icons8.com/?size=48&id=23243&format=png" alt="avatar logo" />
+      
+    </div>
+  )
+}
+
+export default Navbar
