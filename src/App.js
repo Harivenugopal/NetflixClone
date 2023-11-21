a481744192972032902320501faad69d888ba7b1
9072548893 <137638716+9072548893@users.noreply.github.com>
2023-11-21 18:44:23 +0530

    BANNER AND DESCRIPTION ADDED

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import './app.css'
+import Banner from './Banner/Banner';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <Banner/>
     </div>
   );
 }
--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Banner.css'
 
 function Banner() {
   return (
@@ -9,9 +10,11 @@ function Banner() {
             <button className='buttonn'>play</button>
             <button className='buttonn'>mylist</button>
         </div>
-        <h1 className='description'>In a small coastal town nestled between rolling hills and the vast expanse of the ocean, life unfolded at its own leisurely pace. The cobblestone streets meandered through the heart of the community, lined with charming cottages adorned with colorful flower gardens. Each morning, the aroma of freshly brewed coffee wafted through the air from the local café, inviting residents to start their day with a warm cup and friendly conversation. As the sun dipped below the horizon, the sky transformed into a canvas of vibrant hues, casting a soft glow over the tranquil waters. This town, with its close-knit community and picturesque surroundings, was a haven where time seemed to slow down, allowing its residents to savor the simple joys of life</h1>
-
+        <h4 className='description'>In a small coastal town nestled between rolling hills and the vast expanse of the ocean,
+         life unfolded at its own leisurely pace. The cobblestone streets meandered through the heart of the community,</h4>
      </div>
+     <div className='fadeBottam'></div>
+    
 
       
     </div>
