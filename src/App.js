1d583edd390c3965fcbce0e93eea55d1b7e8bb57
9072548893 <137638716+9072548893@users.noreply.github.com>
2023-11-22 10:56:08 +0530

    title card moviessample added(rowpost div name)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import './app.css'
 import Banner from './Banner/Banner';
+import Rowpost from './components/Rowpost/Rowpost';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Banner/>
+    <Rowpost/>
     </div>
   );
 }
---
+++ src/components/Rowpost/Rowpost.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Rowpost.css'
+
+function Rowpost() {
+  return (
+    <div className='row'>
+      <h2>Title</h2>
+      <div className="rowposters">
+        <img className='poster' src="https://wallpaperaccess.com/full/2120936.jpg" alt="poster" />
+        <img className='poster' src="https://wallpaperaccess.com/full/2120936.jpg" alt="poster" />
+        <img className='poster' src="https://wallpaperaccess.com/full/2120936.jpg" alt="poster" />
+        <img className='poster' src="https://wallpaperaccess.com/full/2120936.jpg" alt="poster" />
+        <img className='poster' src="https://wallpaperaccess.com/full/2120936.jpg" alt="poster" />
+        <img className='poster' src="https://wallpaperaccess.com/full/2120936.jpg" alt="poster" />
+        <img className='poster' src="https://wallpaperaccess.com/full/2120936.jpg" alt="poster" />
+        <img className='poster' src="https://wallpaperaccess.com/full/2120936.jpg" alt="poster" />
+      </div>
+    </div>
+  )
+}
+
+export default Rowpost
