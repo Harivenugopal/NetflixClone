51c8ecd6a91ffa698980474e26aed5cdd1a00911
9072548893 <137638716+9072548893@users.noreply.github.com>
2023-12-08 16:03:25 +0530

    secondpage all style almost done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import  avatar from '../Assests/avatar.png'
+import netflixlogo from '../Assests/netflixlogo.png'
+import searchicon from '../Assests/searchicon.png'
+import notification from '../Assests/notification.png'
 import "./Navbar.css";
+
 
 function Navbar(){
   return (
     <div className='navbar'>
-        <button type="button" className='home'>Home </button>
-        <img className='logo' src="https://img.icons8.com/?size=48&id=20519&format=png" alt="netflix logo" />
-        <img className='avatar' src="https://img.icons8.com/?size=48&id=23243&format=png" alt="avatar logo" />
+        <button type="button" className='home'>HOME </button>
+        <button type="button" className='movieslist'>MOVIES</button>
+
+        <img className='logo' src={netflixlogo} alt="netflix logo" />
+        <img className='avatar' src={avatar} alt="avatar logo" />
         <div className="searchContainer">
-        <input type="text" placeholder='Search...' name='Search' className='netflixsearch' />
-        <button type='submit' className='submitbutton'>Submit</button>
-         <button className='notification'><img src="https://icons8.com/icon/fcG5IOptfh58/bell" alt="notification" /></button>
+        <input type="text" placeholder='    Search...' name='Search' className='netflixsearch' />
+        <img   className='submitbutton' src={searchicon} alt="search icon" />
+         <img  className='notification' src={notification} alt="" />
       </div>
     </div>
   )
