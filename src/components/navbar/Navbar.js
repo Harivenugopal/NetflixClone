a1f3ba6e8598c4ff67baf49968d24580357260c5
9072548893 <137638716+9072548893@users.noreply.github.com>
2023-12-08 17:33:07 +0530

    comit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,26 +1,45 @@
-import React from 'react'
-import  avatar from '../Assests/avatar.png'
-import netflixlogo from '../Assests/netflixlogo.png'
-import searchicon from '../Assests/searchicon.png'
-import notification from '../Assests/notification.png'
-import "./Navbar.css";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import avatar from '../Assests/avatar.png';
+import netflixlogo from '../Assests/netflixlogo.png';
+import searchicon from '../Assests/searchicon.png';
+import notification from '../Assests/notification.png';
+import './Navbar.css';
 
+function Navbar() {
+  const navigate = useNavigate();
 
-function Navbar(){
+  const onHomeClick = () => {
+    navigate('/secondpage');
+  };
+
+  const onMoviesClick = () => {
+    navigate('/');
+  };
+
   return (
-    <div className='navbar'>
-        <button type="button" className='home'>HOME </button>
-        <button type="button" className='movieslist'>MOVIES</button>
+    <div className="navbar">
+      <button type="button" className="home" onClick={onHomeClick}>
+        HOME
+      </button>
+      <button type="button" className="movieslist">
+        MOVIES
+      </button>
 
-        <img className='logo' src={netflixlogo} alt="netflix logo" />
-        <img className='avatar' src={avatar} alt="avatar logo" />
-        <div className="searchContainer">
-        <input type="text" placeholder='    Search...' name='Search' className='netflixsearch' />
-        <img   className='submitbutton' src={searchicon} alt="search icon" />
-         <img  className='notification' src={notification} alt="" />
+      <img className="logo" src={netflixlogo} alt="netflix logo" onClick={onMoviesClick} />
+      <img className="avatar" src={avatar} alt="avatar logo" />
+      <div className="searchContainer">
+        <input
+          type="text"
+          placeholder="    Search..."
+          name="Search"
+          className="netflixsearch"
+        />
+        <img className="submitbutton" src={searchicon} alt="search icon" />
+        <img className="notification" src={notification} alt="" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
